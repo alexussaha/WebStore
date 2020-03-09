2d734bbcfbc3d6728cb3a13d9d5d7e6183414460
Александр Усов <1032186060@pfur.ru>
2020-03-09 14:09:29 +0300

    Сделал все по 2-му уроку

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                SurName = "Иванов",
+                FirstName = "Иван",
+                Patronymic = "Иванович",
+                Age = 39,
+                Position = "Администратор"
+            },
+            new Employee
+            {
+                Id = 2,
+                SurName = "Петров",
+                FirstName = "Пётр",
+                Patronymic = "Петрович",
+                Age = 18,
+                Position = "Менеджер"
+            },
+            new Employee
+            {
+                Id = 3,
+                SurName = "Сидоров",
+                FirstName = "Сидор",
+                Patronymic = "Сидорович",
+                Age = 27,
+                Position = "Администратор"
+            },
+        };
+
+        public IActionResult Index()
+        {
+           return View(_Employees);
+        }
+        public IActionResult Details(int Id)
+        {
+            var employee = _Employees.FirstOrDefault(e => e.Id == Id);
+            if (employee is null)
+                return NotFound();
+            return View(employee);
+        }
+    }
+}
--- WebStore/Models/Employee.cs
+++ WebStore/Models/Employee.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebStore.Models
+﻿namespace WebStore.Models
 {
     public class Employee
-    {
+    { 
         public int Id { get; set; }
-        public string Surname { get; set; }
-        public string Name { get; set; }
-        public string Patronymus { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        public string Patronymic { get; set; }
+
         public int Age { get; set; }
+
         public string Position { get; set; }
     }
 }
