593b6b780eb7001e4b6b64069c9199f06771fb47
Александр Усов <1032186060@pfur.ru>
2020-03-12 20:54:32 +0300

    рефакторинг

---
+++ WebStore/Infrastructure/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryEmployeesData : IEmployeeData
+    {
+        private readonly List<Employee> __Employees = TestData.Employees;
+
+        public IEnumerable<Employee> GetAll() => __Employees;
+
+        public Employee GetById(int id) => __Employees.FirstOrDefault(e => e.Id == id);
+
+        public void Add(Employee Employee)
+        {
+            if (Employee is null)
+                throw new ArgumentNullException(nameof(Employee));
+
+            if (__Employees.Contains(Employee)) return;
+            Employee.Id = __Employees.Count == 0 ? 1 : __Employees.Max(e => e.Id) + 1;
+            __Employees.Add(Employee);
+        }
+
+        public void Edit(int id, Employee Employee)
+        {
+            if (Employee is null)
+                throw new ArgumentNullException(nameof(Employee));
+
+            if (__Employees.Contains(Employee)) return;
+
+            var db__Employee = GetById(id);
+            if (db__Employee is null)
+                return;
+
+            db__Employee.SurName = Employee.SurName;
+            db__Employee.FirstName = Employee.FirstName;
+            db__Employee.Patronymic = Employee.Patronymic;
+            db__Employee.Age = Employee.Age;
+        }
+
+        public bool Delete(int id)
+        {
+            var db__Employee = GetById(id);
+            if (db__Employee is null)
+                return false;
+
+            return __Employees.Remove(db__Employee);
+        }
+
+        public void SaveChanges() { }
+    }
+}
