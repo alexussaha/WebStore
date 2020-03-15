82615dc92859c525f96ec5008309b557c5ccdd45
Александр Усов <1032186060@pfur.ru>
2020-03-15 18:13:55 +0300

    Бренды

---
+++ WebStore/ViewModels/BrandViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domian.Entities.Base.Interfaces;
+
+namespace WebStore.ViewModels
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Order { get; set; }
+    }
+}
