adbd48f5c57b22d48f1b42dec2af9c4ce517c937
Александр Усов <ussov39@gmail.com>
2020-04-06 17:05:47 +0300

    Перенос модели и модели представлений в проект Предметной области

---
+++ Common/WebStore.Domian/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WebStore.Domian.Entities.Base.Interfaces;
+
+namespace WebStore.Domian.ViewModels
+{
+    public class ProductViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+        public string Brand { get; internal set; }
+    }
+}
