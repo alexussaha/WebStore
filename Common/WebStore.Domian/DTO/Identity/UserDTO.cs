71024fbeb94025242583cd3ae178a1d74883f6db
Александр Усов <ussov39@gmail.com>
2020-04-11 11:10:24 +0300

    DTO модели для WebAPI системы Identity

---
+++ Common/WebStore.Domian/DTO/Identity/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domian.Entities.Identity;
+
+namespace WebStore.Domian.DTO.Identity
+{
+    public abstract class UserDTO
+    {
+        public User User { get; set; }
+    }
+
+    public class AddLoginDTO : UserDTO
+    {
+        public UserLoginInfo UserLoginInfo { get; set; }
+    }
+
+    public class PasswordHashDTO : UserDTO
+    {
+        public string Hash { get; set; }
+    }
+
+    public class SetLockoutDTO : UserDTO
+    {
+        public  DateTimeOffset? LockoutEnd { get; set; }
+    }
+}
