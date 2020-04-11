2dc53c5f1503878f2d3f710fd4cd7a3214dec788
Александр Усов <ussov39@gmail.com>
2020-04-11 10:54:39 +0300

    Интерфейсы, требующие реализации

---
+++ Services/WebStore.Interfaces/Services/IRoleClient.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domian.Entities.Identity;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IRoleClient: IRoleStore<Role>{ }
+}
---
+++ Services/WebStore.Interfaces/Services/IUserClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using WebStore.Domian.Entities.Identity;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IUserClient : IUserRoleStore<User>, IUserPasswordStore<User>, IUserEmailStore<User>, IUserPhoneNumberStore<User>, IUserTwoFactorStore<User>, IUserLockoutStore<User>, IUserClaimStore<User>, IUserLoginStore<User>
+    {
+    }
+}
