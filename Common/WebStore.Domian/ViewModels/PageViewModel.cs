4052601eaec1d1e7927852f7f052f50904346dd3
Александр Усов <ussov39@gmail.com>
2020-05-01 10:15:44 +0300

    Модели представления для разбиения страницы

---
+++ Common/WebStore.Domian/ViewModels/PageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domian.ViewModels
+{
+    
+        public class PageViewModel
+        {
+            public int TotalItems { get; set; }
+
+            public int PageSize { get; set; }
+
+            public int PageNumber { get; set; }
+
+            public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / PageSize);
+        }
+    
+}
