85c69e6630e35e536121a665c2d054e219f7e8eb
Александр Усов <ussov39@gmail.com>
2020-05-04 16:04:33 +0300

    Таг- хелпер генерации визуальной разбивки на страницы

--- UI/WebStore/TagHelpers/PagingTagHelper.cs
+++ UI/WebStore/TagHelpers/PagingTagHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
@@ -11,44 +12,44 @@ namespace WebStore.TagHelpers
 {
     public class PagingTagHelper : TagHelper
     {
-        private readonly IUrlHelperFactory _UrlHelperFactory;
+            [ViewContext, HtmlAttributeNotBound]
+            public ViewContext ViewContext { get; set; }
 
-        [ViewContext, HtmlAttributeNotBound]
-        public ViewContext ViewContext { get; set; }
+            public PageViewModel PageModel { get; set; }
 
-        public PageViewModel PageModel { get; set; }
+            public string PageAction { get; set; }
 
-        public string PageAction { get; set; }
+            [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+            public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 
-        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
-        public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            public override void Process(TagHelperContext context, TagHelperOutput output)
+            {
+                var ul = new TagBuilder("ul");
+                ul.AddCssClass("pagination");
 
-        public PagingTagHelper(IUrlHelperFactory UrlHelperFactory) => _UrlHelperFactory = UrlHelperFactory;
+                for (int i = 1, total_pages = PageModel.TotalPages; i < total_pages; i++)
+                    ul.InnerHtml.AppendHtml(CreateItem(i));
 
-        public override void Process(TagHelperContext context, TagHelperOutput output)
-        {
-            var url_helper = _UrlHelperFactory.GetUrlHelper(ViewContext);
+                output.Content.AppendHtml(ul);
+            }
 
-            var ul = new TagBuilder("ul");
-            ul.AddCssClass("pagination");
-
-            for (int i = 1, total_pages = PageModel.TotalPages; i < total_pages; i++)
-                ul.InnerHtml.AppendHtml(CreateItem(i, url_helper));
-
-            output.Content.AppendHtml(ul);
-        }
-
-        private TagBuilder CreateItem(int PageNumber, IUrlHelper Url)
+            private TagBuilder CreateItem(int PageNumber)
         {
             var li = new TagBuilder("li");
             var a = new TagBuilder("a");
 
             if (PageNumber == PageModel.PageNumber)
+            {
+                a.MergeAttribute("data-page", PageModel.PageNumber.ToString());
                 li.AddCssClass("active");
+            }
             else
             {
                 PageUrlValues["page"] = PageNumber;
-                a.Attributes["href"] = Url.Action(PageAction, PageUrlValues);
+                a.Attributes["href"] = "#";
+                foreach (var (key, value) in PageUrlValues.Where(p => p.Value != null))
+                    a.MergeAttribute($"data-{key}", value.ToString());
+
             }
 
             a.InnerHtml.AppendHtml(PageNumber.ToString());
